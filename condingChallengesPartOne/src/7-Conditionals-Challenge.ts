b4461a0d2500c099a8ac89d207226f462e78ba63
Jordan Nitta-Rodrigues <jnittar@wgu.edu>
2023-11-17 10:01:52 -0800

    Finished the coding challenges

--- condingChallengesPartOne/src/7-Conditionals-Challenge.ts
+++ condingChallengesPartOne/src/7-Conditionals-Challenge.ts
@@ -2,6 +2,9 @@
 Return a string “Congratulations” if a person is equal to or older than 21
 */
 
-// ----------------------------------------------------
-// *-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-*-
-// ----------------------------------------------------
+let age: number = 21;
+if (age >= 21) {
+    console.log("Congratulations");
+}
+
+
